f8dc7e06dbb658d94c4ba25ac0f59af052c2c715
vasilsariev <vasil.sariev12@gmail.com>
2026-02-26 23:00:37 +0200

    another 10 exercises

--- exercises/03_ticket_v1/02_validation/src/lib.rs
+++ exercises/03_ticket_v1/02_validation/src/lib.rs
@@ -18,7 +18,24 @@ impl Ticket {
     // as well as some `String` methods. Use the documentation of Rust's standard library
     // to find the most appropriate options -> https://doc.rust-lang.org/std/string/struct.String.html
     fn new(title: String, description: String, status: String) -> Self {
-        todo!();
+        let max_title_len = 50;
+        let max_description_len = 500;
+        if !&status.eq("To-Do") && !&status.eq("In Progress") && !&status.eq("Done") {
+            panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
+        }
+        if title.is_empty() {
+            panic!("Title cannot be empty");
+        }
+        if description.is_empty() {
+            panic!("Description cannot be empty");
+        }
+
+        if title.len() > max_title_len {
+            panic!("Title cannot be longer than 50 bytes");
+        }
+        if description.len() > max_description_len {
+            panic!("Description cannot be longer than 500 bytes");
+        }
         Self {
             title,
             description,
--- exercises/03_ticket_v1/07_setters/src/lib.rs
+++ exercises/03_ticket_v1/07_setters/src/lib.rs
@@ -11,21 +11,9 @@ pub struct Ticket {
 
 impl Ticket {
     pub fn new(title: String, description: String, status: String) -> Ticket {
-        if title.is_empty() {
-            panic!("Title cannot be empty");
-        }
-        if title.len() > 50 {
-            panic!("Title cannot be longer than 50 bytes");
-        }
-        if description.is_empty() {
-            panic!("Description cannot be empty");
-        }
-        if description.len() > 500 {
-            panic!("Description cannot be longer than 500 bytes");
-        }
-        if status != "To-Do" && status != "In Progress" && status != "Done" {
-            panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
-        }
+        check_title(&title);
+        check_description(&description);
+        check_status(&status);
 
         Ticket {
             title,
@@ -44,6 +32,43 @@ impl Ticket {
 
     pub fn status(&self) -> &String {
         &self.status
+    }
+
+    pub fn set_title(&mut self, title: String) {
+        check_title(&title);
+        self.title = title;
+    }
+    pub fn set_description(&mut self, description: String) {
+        check_description(&description);
+        self.description = description;
+    }
+    pub fn set_status(&mut self, status: String) {
+        check_status(&status);
+        self.status = status;
+    }
+}
+
+fn check_title(title: &str) {
+    if title.is_empty() {
+        panic!("Title cannot be empty");
+    }
+    if title.len() > 50 {
+        panic!("Title cannot be longer than 50 bytes");
+    }
+}
+
+fn check_description(description: &str) {
+    if description.is_empty() {
+        panic!("Description cannot be empty");
+    }
+    if description.len() > 500 {
+        panic!("Description cannot be longer than 500 bytes");
+    }
+}
+
+fn check_status(status: &str) {
+    if status != "To-Do" && status != "In Progress" && status != "Done" {
+        panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
     }
 }
 
